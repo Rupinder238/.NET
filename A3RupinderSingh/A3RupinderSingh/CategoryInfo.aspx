6f8b03d0a8f80ab9fa4d414d3ee57bc4c2550783
Rupinder Singh <is.rupindersingh2381996@gmail.com>
2018-11-08 11:44:23 -0500

    Add files via upload

---
+++ A3RupinderSingh/A3RupinderSingh/CategoryInfo.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CategoryInfo.aspx.cs" Inherits="A3RupinderSingh.A3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        body {
+    background-color: lightskyblue;
+}
+        h1,h2,h3 {
+    color: red;
+    font-family: "Freestyle Script";
+    font-size:30px;
+}
+
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+        </div>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <h1>Select the Category from the list below</h1>
+                <asp:ListBox ID="Category" runat="server" Height="169px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="246px" AutoPostBack="True"></asp:ListBox><br /><br />
+                <h3>Select the product from the dropdown</h3>
+                <asp:DropDownList ID="Product" runat="server" AutoPostBack="True" Height="16px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="196px">
+                </asp:DropDownList><br /><br />
+                <h2>Table below is the orders for the Selected product</h2>
+                <asp:GridView ID="order" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                </asp:GridView>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ A3RupinderSingh/A3RupinderSingh/CategoryInfo.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace A3RupinderSingh
+{
+    public partial class A3 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["NorthWind"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string query = "Select CategoryID,CategoryName from Categories;";
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Category.DataSource = reader;
+                    Category.DataTextField = "CategoryName";
+                    Category.DataValueField = "CategoryID";
+                    Category.DataBind();
+                }
+                ListItem liSelect = new ListItem("Select a Product", "-1");
+                Product.Items.Insert(0, liSelect);
+                Product.Enabled = false;
+            }
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string query = "Select ProductID, ProductName from Products where CategoryID = "+ Category.SelectedValue + ";";
+            Product.Enabled = true;
+            order.DataSource = null;
+            order.DataBind();
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                Product.DataSource = reader;
+                Product.DataTextField = "ProductName";
+                Product.DataValueField = "ProductID";
+                Product.DataBind();
+            }
+            ListItem liSelect = new ListItem("Select a Product", "-1");
+            Product.Items.Insert(0, liSelect);
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = "Select Orders.OrderID, OrderDate, ShippedDate, ShipName, ShipAddress, ShipCity, ShipCountry from Orders inner join [Order Details] on Orders.OrderID  = [Order Details].OrderID where [Order Details].ProductID = "+ Product.SelectedValue +";";
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                order.DataSource = reader;
+                order.DataBind();
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
